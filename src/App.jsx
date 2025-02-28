8d705cc3014574cca19417532f78c46a108ec6da
akram-abid <a.abid44778@gmail.com>
2025-02-28 18:31:40 +0100

    do: assign one task to the CardGallery component to get a mount of cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,62 +2,29 @@ import { useState, useEffect } from 'react';
 import { getCardData } from './data/cardsData';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [cards, setCards] = useState([]);
   return (
     <>
-      <CardGallery />
-      <img 
-        src="https://s4.anilist.co/file/anilistcdn/character/large/b17-IazKGogQwJ1p.png" 
-        alt="fvs" 
-      />
+      <CardGallery numberOfCards={12} setCards={setCards}/>
     </>
   );
 }
 
-export default function CardGallery() {
-  const [cards, setCards] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+function CardGallery({ numberOfCards , setCards}) {
 
   useEffect(() => {
-    const numberOfCards = 30;
-
     const fetchCards = async () => {
       try {
-        setLoading(true);
         const result = await getCardData(numberOfCards);
         setCards(result);
+        console.log("Fetched Cards:", result); // Log the result here
       } catch (err) {
-        setError("Failed to load cards");
-      } finally {
-        setLoading(false);
+        throw new Error("no cards to retrive something went wrong")
       }
     };
 
     fetchCards();
-  }, []);
-
-  console.log("finnaly ",cards[0])
-  
-  return (
-    <div>
-      {loading && <p>Loading cards...</p>}
-      {error && <p>{error}</p>}
-      {cards.length > 0 ? (
-        <>
-          <h1>Card Gallery</h1>
-          <div className="card-gallery">
-            {cards.map((card, index) => (
-              <div key={index} className="card">
-                <img src={card.url} alt={card.name} height={"300px"}/>
-                <p>{card.name}</p>
-              </div>
-            ))}
-          </div>
-        </>
-      ) : (
-        !loading && <div>No cards available</div>
-      )}
-    </div>
-  );
+    
+  }, [numberOfCards, setCards]);
 }
